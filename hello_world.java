cf2dedb438820a0175ac750aca8c9d83f589c7d6
sean15535 <sean15535@gmail.com>
2024-06-22 23:38:42 +0100

    "Fixed class name capitalization, corrected 'Sytring' typo, and added newline at end of file."

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,5 @@
-public class Hello_world{
-    public static void main (Sytring [] args){
-        System.out.println("Hello World!")
+public class hello_world{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
- 
