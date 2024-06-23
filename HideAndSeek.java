b410068005bb6811cde7379bb0ce38698f870701
sean15535 <sean15535@gmail.com>
2024-06-23 11:40:31 +0100

    HideAndSeek.java code"

---
+++ HideAndSeek.java
@@ -0,0 +1,9 @@
+public class HideAndSeek{
+	public static void main(String [] args) {
+		System.out.println("Lets Play Hide and Seek");
+		System.out.println("Three...");
+		System.out.println("Two...");
+		System.out.println("One...");
+		System.out.println("Ready or not, here i come 🙂 ");
+	}
+}
