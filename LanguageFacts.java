6c5f8850aeaa3db0f847ef9e52fbce277ea47bee
sean15535 <sean15535@gmail.com>
2024-06-23 14:32:10 +0100

    language facts

---
+++ LanguageFacts.java
@@ -0,0 +1,8 @@
+public class LanguageFacts {
+    public static void main (String[] args){
+        System.out.println("Java is a class-based Language");
+        System.out.println("Java classes have a main method. ");
+        System.out.println("Java statement ends with semi colon");
+        System.out.println("Programming is.. fun!");
+    }
+}
