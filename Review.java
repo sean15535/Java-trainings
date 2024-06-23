d1b6381538d0b62420da9511af7c93b172be1f9c
sean15535 <sean15535@gmail.com>
2024-06-23 14:59:57 +0100

    review code

---
+++ Review.java
@@ -0,0 +1,7 @@
+public class Review {
+    public static void main(String[] args){
+        // the main method executes the tasks of the class
+        System.out.print("My first Java program from scratch! .");
+
+    }
+}
