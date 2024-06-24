7a2c5f879c392e5fd6469f4bcde642c75339e1f3
sean15535 <sean15535@gmail.com>
2024-06-24 20:01:44 +0100

    quiz game . java

---
+++ QuizGame.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class QuizGame {
+    public static void main (String[] args){
+        // Array of questions
+        String[] questions = {
+                "1. What is the capital of France?\nA) Paris\nB) London\nC) Berlin\nD) Madrid",
+                "2. What is 2 + 2?\nA) 3\nB) 4\nC) 5\nD) 6",
+                "3. Who wrote 'Hamlet'?\nA) Charles Dickens\nB) Jane Austen\nC) William Shakespeare\nD) Mark Twain",
+                "4. What is the largest planet in our solar system?\nA) Earth\nB) Mars\nC) Jupiter\nD) Saturn",
+                "5. What is the chemical symbol for water?\nA) CO2\nB) H2O\nC) O2\nD) NaCl"
+        };
+    }
+}
