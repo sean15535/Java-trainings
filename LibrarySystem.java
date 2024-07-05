27a71d4827be7ef640b735b8eb518582dc629a59
sean15535 <sean15535@gmail.com>
2024-07-05 04:25:39 +0100

    LibrarySystem.java

---
+++ LibrarySystem.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Book {
+    String title;
+    String author;
+    int quantity;
+
+    Book(String title, String author, int quantity) {
+        this.title = title;
+        this.author = author;
+        this.quantity = quantity;
+    }
+}
